ce250b1a4679d1480b23cb50b6b37f95151ccb46
wkrlsk23 <wkrlsk23@gmail.com>
2020-04-06 21:53:12 +0900

    Update JeongJongHo.py

--- 6 April/JeongJongHo.py
+++ 6 April/JeongJongHo.py
@@ -1,5 +1,24 @@
 class Solution(object):
     def groupAnagrams(self, strs):
+        #print(len(strs))
+        anagrams = []
+        output = []
+        for i in strs:
+            isan = False
+            word = sorted(i)
+            for j in anagrams :
+                if j == word:
+                    output[anagrams.index(word)].append(i)
+                    isan = True
+                    break
+            if isan == False :
+                anagrams.append(word)
+                output.append([i])
+        return output
+
+
+class Solution(object):
+    def groupAnagrams(self, strs):
         o = collections.defaultdict(list)
         for i in strs:
             word = sorted(i)
