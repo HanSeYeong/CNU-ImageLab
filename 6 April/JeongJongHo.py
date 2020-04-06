b6f7393e4cee4a48198b5543f35342b1874c78d9
wkrlsk23 <wkrlsk23@gmail.com>
2020-04-06 21:52:44 +0900

    Update JeongJongHo.py

--- 6 April/JeongJongHo.py
+++ 6 April/JeongJongHo.py
@@ -1,17 +1,12 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        #print(len(strs))
-        anagrams = []
-        output = []
+        o = collections.defaultdict(list)
         for i in strs:
-            isan = False
             word = sorted(i)
-            for j in anagrams :
-                if j == word:
-                    output[anagrams.index(word)].append(i)
-                    isan = True
-                    break
-            if isan == False :
-                anagrams.append(word)
-                output.append([i])
-        return output
+            o[tuple(word)].append(i)
+        return o.values()
+    
+    
+    #https://www.developer.com/lang/other/article.php/630941/Learn-to-Program-using-Python-Using-Tuples-as-Keys.htm
+    #https://www.daleseo.com/python-collections-defaultdict/
+        
