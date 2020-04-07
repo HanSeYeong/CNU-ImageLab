0dfe65997eb2a13abd20fa61c07729fbe92cab30
wkrlsk23 <wkrlsk23@gmail.com>
2020-04-07 19:23:37 +0900

    Changed!

--- bat_file/changing.bat
+++ bat_file/changing.bat
@@ -3,7 +3,6 @@ cd %github_folder%
 :Start
 set /p message=Ä¿¹ÔÇÒ ³»¿ë : 
 IF "%message%"=="" GOTO Default
-set message=%date% %message%
 git pull
 git commit -m "%message%" -a
 git push
@@ -12,7 +11,6 @@ GOTO Exit
 
 :Default
 set msg=Changed!
-set msg=%date% %msg%
 git pull
 git commit -m "%msg%" -a
 git push
--- bat_file/file_upload.bat
+++ bat_file/file_upload.bat
@@ -5,7 +5,6 @@ git add %filename%
 :Start
 set /p message=Ä¿¹ÔÇÒ ³»¿ë : 
 IF "%message%"=="" GOTO Default
-set message=%date% %message%
 git pull
 git commit -m "%message%" -a
 git push
@@ -14,7 +13,6 @@ GOTO Exit
 
 :Default
 set msg=Changed!
-set msg=%date% %msg%
 git pull
 git commit -m "%msg%" -a
 git push
