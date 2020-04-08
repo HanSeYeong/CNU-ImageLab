629caddc74c8341884bd71348b7d15f881240dd3
ChangSeok-Lee <55068099+ChangSeok-Lee@users.noreply.github.com>
2020-04-08 21:34:39 +0900

    Create Changseok.cpp
    0m / 6MB

---
+++ 8 April_Middle of Linked LIst/Changseok.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+       ListNode* temp;
+        temp = head;
+        int n=1;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+            n++;
+        }
+
+        n =n/2+1;
+        
+        temp=head;
+        for(int i = 1; i< n; i++)
+        {
+            temp  = temp->next;
+        }
+        
+        return temp;
+    }
+};
