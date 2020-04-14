36639bff40b87738cb108b648d66992550f9d1a9
wkrlsk23 <wkrlsk23@gmail.com>
2020-04-14 16:30:32 +0900

    title changed!

--- _12 April Last Stone Weight/seyeong.py
+++
@@ -1,8 +0,0 @@
-def lastStoneWeight(stones):
-    while True:
-        if len(stones) > 1:
-            stones.sort()
-            stones[-1] -= stones[-2]
-            del stones[-2]
-        else:
-            return stones[0]
